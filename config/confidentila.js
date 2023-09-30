9c832a999922621e5e4278a57c7fe30862b2ab46
sanjeevsingh20 <sk2747267@gmail.com>
2023-09-30 12:25:58 +0530

    my express api

--- config/confidentila.js
+++ config/confidentila.js
@@ -3,6 +3,7 @@ const email_user = "manojkumarsinghy20@gmail.com";
 const secret_key = "yametekhudasai";
 const data_password = "@#Sanjukumar123";
 const uri ="mongodb+srv://taskHero:TaskHeroSanjeev123@cluster0.jwq5ndx.mongodb.net/?retryWrites=true&w=majority"
+// const uri ="mongodb://localhost:27017/Notebook"
 
 module.exports = {
   emai_password,
--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const connectmongo = require("./database");
+const connectmongp = require("./database");
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.use(
   cors({
     origin: "*",
   })
 );
-connectmongo();
+connectmongp();
 app.use(express.json());
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
@@ -16,9 +16,9 @@ app.use("/api/notes", require("./routes/notes"));
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
- 
+
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
 });
 
-module.exports = app
+module.exports = app;
